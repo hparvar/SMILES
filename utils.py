0089069aa78cb7cd69e6d332300188adb8ba285e
hparvar <hparvar@gmail.com>
2020-08-04 16:48:30 -0400

    cleaning the code and put functions into the util.py file

--- utils.py
+++ utils.py
@@ -11,8 +11,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.utils.multiclass import unique_labels
+from sklearn.model_selection import train_test_split
 
+from feature_extractor import fingerprint_features
 from rdkit.Chem import AllChem
+from rdkit import Chem
+def load_data(filename, targets='P1'):
+    df = pd.read_csv(filename)
+    return df.smiles, df[targets]
+
+def split_data(features, target, val_size=0.1, test_size=0.1, random_state=42):
+    X, X_test, y, y_test = train_test_split(features, target, test_size=test_size, random_state=random_state)
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=val_size, random_state=random_state)
+    return X_train, X_val, X_test, y_train, y_val, y_test
 
 def ExplicitBitVect_to_NumpyArray(bitvector):
     bitstring = bitvector.ToBitString()
@@ -60,6 +71,32 @@ def mol2alt_sentence(mol, radius=2):
 
     return list(alternating_sentence)
 
+def extract_features(smiles):
+    features = []
+    for i in range(len(smiles)):
+        extracted_example = fingerprint_features(smiles.iloc[i])
+        features.append(ExplicitBitVect_to_NumpyArray(extracted_example))
+    # return features
+    return np.vstack(features)
+
+def map_moleculs(smiles):
+    mols = [Chem.MolFromSmiles(s) for s in smiles]
+    mol_sentences = [mol2alt_sentence(x) for x in mols]
+    vocab = np.unique([x for l in mol_sentences for x in l])  # array of unique substructures (Morgan identifiers)
+    num_words = len(vocab)  # number of unique substructures
+    # Create a mapping of Morgan identifiers to integers between 1 and num_words
+    word_map = dict()
+    for i in range(len(vocab)):
+        word_map[vocab[i]] = i+1
+    # mol_map is like mol_sentences but Morgan identifiers are replaced by their value in wordMap
+    mol_map = []
+    for m in mol_sentences:
+        mol_map.append([word_map[s] for s in m])
+    mol_map = np.array(mol_map, dtype=object)
+    
+    max_seq_len = max([len(mol_map[i]) for i in range(len(mol_map))])  # length of longest molecule
+    
+    return mol_map, word_map, max_seq_len, vocab, num_words
 
 def plot_training_results(history, model_name_path):
     # Plotting the Train Valid Loss Graph
