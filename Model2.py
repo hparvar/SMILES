cfe837c14310f146b38cc9b17a67a2bd13b6df95
hparvar <hparvar@gmail.com>
2020-08-04 16:48:14 -0400

    cleaning the code and put functions into the util.py file

--- Model2.py
+++ Model2.py
@@ -5,62 +5,41 @@ Created on Mon Aug  3 17:24:31 2020
 @author: h_par
 """
 
-import pandas as pd
-import numpy as np
-
-from sklearn.model_selection import train_test_split
-
-import tensorflow as tf
 from tensorflow import keras
 
-from rdkit import Chem
-from utils import mol2alt_sentence, show_results, plot_training_results
+from utils import show_results, plot_training_results, load_data, map_moleculs, split_data
 
 
-def load_data_single(filename):
-    df = pd.read_csv(filename)
-    target = df['P1']
-    
-    return df.smiles, target
+# parameters --------------------------------------------
+targets=['P1'] 
+file_name = 'dataset_single.csv'
+lr = 0.00001
+optimizer = keras.optimizers.Adam(lr=lr)
+loss = "categorical_crossentropy"
+batch_size = 32
+epochs = 5
+model_name = 'test_2' # ToDo: Check if the model exist
+embedding_length = 32
 
-def map_moleculs(smiles):
-    mols = [Chem.MolFromSmiles(s) for s in smiles]
-    mol_sentences = [mol2alt_sentence(x) for x in mols]
-    vocab = np.unique([x for l in mol_sentences for x in l])  # array of unique substructures (Morgan identifiers)
-    num_words = len(vocab)  # number of unique substructures
-    # Create a mapping of Morgan identifiers to integers between 1 and num_words
-    word_map = dict()
-    for i in range(len(vocab)):
-        word_map[vocab[i]] = i+1
-    # mol_map is like mol_sentences but Morgan identifiers are replaced by their value in wordMap
-    mol_map = []
-    for m in mol_sentences:
-        mol_map.append([word_map[s] for s in m])
-    mol_map = np.array(mol_map, dtype=object)
-    
-    max_seq_len = max([len(mol_map[i]) for i in range(len(mol_map))])  # length of longest molecule
-    
-    return mol_map, word_map, max_seq_len, vocab, num_words
+# -------------------------------------------------------
 
+smiles, target = load_data(file_name, targets=targets)
+features, word_map, max_seq_len, vocab, num_words = map_moleculs(smiles)
+# Split data to train, validation and test
+X_train, X_val, X_test, y_train, y_val, y_test = split_data(features, target)
 
-smiles, target = load_data_single('dataset_single.csv')
-mol_map, word_map, max_seq_len, vocab, num_words = map_moleculs(smiles)
-
-
-X, X_test, y, y_test = train_test_split(mol_map, target, test_size=0.1, random_state=11)
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.1, random_state=11)
 # Make sequences have same length with 0 padding on both sides
 X_train = keras.preprocessing.sequence.pad_sequences(X_train, maxlen=max_seq_len)
 X_val = keras.preprocessing.sequence.pad_sequences(X_val, maxlen=max_seq_len)
 X_test = keras.preprocessing.sequence.pad_sequences(X_test, maxlen=max_seq_len)
 
+# Convert labels to onehot
 y_train = keras.utils.to_categorical(y_train, num_classes=2)
 y_val = keras.utils.to_categorical(y_val, num_classes=2)
 y_test = keras.utils.to_categorical(y_test, num_classes=2)
   
 
-embedding_length = 32
-model_name = 'model_2' 
+
 # ToDo: Check if the model exist
 
 
@@ -77,12 +56,12 @@ model.summary()
 
 
 #Compiling the model
-model.compile(optimizer=keras.optimizers.Adam(lr=0.00001), loss="categorical_crossentropy", metrics=['accuracy'])
+model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
 
 #Training the model
 lr_reduce = keras.callbacks.ReduceLROnPlateau(min_lr=0.000001)
 mcp_save = keras.callbacks.ModelCheckpoint('model/' + model_name + '.h5', save_best_only=True, monitor='val_loss', mode='min')
-history = model.fit(X_train, y_train, batch_size=32, epochs=100, validation_data=(X_val, y_val), callbacks=[mcp_save, lr_reduce], shuffle=True)
+history = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_val, y_val), callbacks=[mcp_save, lr_reduce], shuffle=True)
 
 plot_training_results(history, 'model/' + model_name)
 
@@ -106,7 +85,7 @@ print("Loaded model from disk")
 
 # evaluate loaded model on test data
 
-loaded_model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(lr=0.0001), metrics=['accuracy'])
+loaded_model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
 score = loaded_model.evaluate(X_train, y_train, verbose=0)
 print("Training %s: %.2f%%" % (loaded_model.metrics_names[1], score[1] * 100))
 score = loaded_model.evaluate(X_val, y_val, verbose=0)
